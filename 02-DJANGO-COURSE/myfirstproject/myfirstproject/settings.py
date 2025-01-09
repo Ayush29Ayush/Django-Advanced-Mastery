7fa6964fbcc5dd5ae30f416556a3d77a265ea08f
Ayush29Ayush <ayushsenapati123@gmail.com>
2025-01-09 22:48:54 +0530

    feat: PostgreSQL database added

--- 02-DJANGO-COURSE/myfirstproject/myfirstproject/settings.py
+++ 02-DJANGO-COURSE/myfirstproject/myfirstproject/settings.py
@@ -76,9 +76,13 @@ ASGI_APPLICATION = "myfirstproject.asgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydjangodatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'Ab@123456',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
