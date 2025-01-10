b2a3febf1dbd043fb8c58c56f8801db4c9d72a10
Ayush29Ayush <ayushsenapati123@gmail.com>
2025-01-10 18:32:08 +0530

    feat: Django urls updated

--- 02-DJANGO-COURSE/myfirstproject/myfirstapp/urls.py
+++ 02-DJANGO-COURSE/myfirstproject/myfirstapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from myfirstapp.views import index, contact, dynamic_route
+from myfirstapp.views import index, contact, dynamic_route, my_django_forms, my_django_model_forms ,my_html_forms
 
 urlpatterns = [
     path("", index, name="index"),
     path("contact/", contact, name="contact"),
     path("dynamic_route/<int:number>", dynamic_route, name="dynamic_route"),
+    path("my_django_forms/", my_django_forms, name="my_django_forms"),
+    path("my_django_model_forms/", my_django_model_forms, name="my_django_model_forms"),
+    path("my_html_forms/", my_html_forms, name="my_html_forms"),
 ]
