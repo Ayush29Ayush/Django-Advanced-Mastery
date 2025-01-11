2faf7d19411db448315ec83b5642e63ea1e6c1e2
Ayush29Ayush <ayushsenapati123@gmail.com>
2025-01-11 22:26:32 +0530

    feat: Django urls updated

--- 02-DJANGO-COURSE/myfirstproject/myfirstapp/urls.py
+++ 02-DJANGO-COURSE/myfirstproject/myfirstapp/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from myfirstapp.views import index, contact, dynamic_route, my_django_forms, my_django_model_forms ,my_html_forms
+from myfirstapp.views import (
+    index,
+    contact,
+    dynamic_route,
+    my_django_forms,
+    my_django_model_forms,
+    my_html_forms,
+    search_page,
+    bulk_operations_view,
+)
 
 urlpatterns = [
     path("", index, name="index"),
@@ -8,4 +17,8 @@ urlpatterns = [
     path("my_django_forms/", my_django_forms, name="my_django_forms"),
     path("my_django_model_forms/", my_django_model_forms, name="my_django_model_forms"),
     path("my_html_forms/", my_html_forms, name="my_html_forms"),
+    
+    path("search_page/", search_page, name="search_page"),
+    
+    path("bulk-operations/", bulk_operations_view, name="bulk_operations"),
 ]
