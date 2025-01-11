af6ba9657902226ff73368c1a9af0e81f2cafdce
Ayush29Ayush <ayushsenapati123@gmail.com>
2025-01-11 22:25:51 +0530

    feat: Django Models updated

--- 02-DJANGO-COURSE/myfirstproject/myfirstapp/models.py
+++ 02-DJANGO-COURSE/myfirstproject/myfirstapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from myfirstapp.utils import generateSlug 
 
 
 class College(models.Model):
@@ -14,6 +15,7 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     mobile_number = models.CharField(max_length=12)
     email = models.EmailField()
+    age = models.IntegerField(default=18)
     gender = models.CharField(max_length=10, choices=gender_choices, default="Male")
     student_bio = models.TextField()
     date_of_birth = models.DateField(blank=True, null=True)
@@ -72,8 +74,8 @@ class Skills(models.Model):
     skill_name = models.CharField(max_length=100)
     is_deleted = models.BooleanField(default=False)
 
-    objects = SkillManager()
-    new_manager = models.Manager()
+    objects = SkillManager() #! Setting SkillManager as default manager
+    new_manager = models.Manager() #! Creating new manager and setting old default manager here
 
     def __str__(self):
         return self.skill_name
@@ -83,8 +85,20 @@ class Person(models.Model):
     person_name = models.CharField(max_length=100)
     skill = models.ManyToManyField(Skills)
 
+
 class Student2(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     gender = models.CharField(max_length=100, null=True, blank=True)
     upload_file = models.FileField(null=True, blank=True, upload_to="files/")
+
+
+class Products(models.Model):
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, blank=True)
+    product_name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, null=True, blank=True)
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.id:
+            self.slug = generateSlug(self.product_name, Products)
+        return super().save(*args, **kwargs)
