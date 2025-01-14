3ecdec392a47649b53aea286ca85bd5a34c08d99
Ayush29Ayush <ayushsenapati123@gmail.com>
2025-01-14 07:50:38 +0530

    feat: Functionality to check custom headers added

--- 02-DJANGO-COURSE/djangomiddleware/home/views.py
+++ 02-DJANGO-COURSE/djangomiddleware/home/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from home.models import Store
 
 
 # Create your views here.
 def index(request):
-    data = {"status": "success", "message": "Hello, Django!"}
+
+    store = Store.objects.get(bmp_id=request.headers.get("bmp_id"))
+
+    headers = dict(request.headers)
+    data = {
+        "status": "success",
+        "message": "Hello, Django!",
+        "headers": headers,
+        "store": {"bmp_id": store.bmp_id, "store_name": store.store_name},
+    }
     return JsonResponse(data)
