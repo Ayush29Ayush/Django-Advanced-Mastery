9166bf9fac8295f77362e6d7e425aa6e2dc3b54e
Ayush29Ayush <ayushsenapati123@gmail.com>
2025-01-18 20:21:47 +0530

    feat: Implemented Rate Limiting

--- 02-DJANGO-COURSE/djangoauthentication/accounts/views.py
+++ 02-DJANGO-COURSE/djangoauthentication/accounts/views.py
@@ -8,6 +8,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.core.cache import cache
 
 User = get_user_model()
 
@@ -15,6 +16,21 @@ User = get_user_model()
 def login_page(request):
     if request.method == "POST":
         phone_number = request.POST.get("phone_number")
+        if cache.get(phone_number):
+            data = cache.get(phone_number)
+            print(data)
+            data["count"] += 1
+            if data["count"] >= 3:
+                cache.set(phone_number, data, 60 * 5)
+                messages.error(request, "You can request OTP after 5 mins.")
+                return redirect("/")
+
+            cache.set(phone_number, data, 60 * 1)
+
+        if not cache.get(phone_number):
+            data = {"phone_number": phone_number, "count": 1}
+            cache.set(phone_number, data, 60 * 1)
+
         user_obj = User.objects.filter(phone_number=phone_number)
         if not user_obj.exists():
             return redirect("/")
@@ -35,6 +51,11 @@ def check_otp(request, user_id):
     if request.method == "POST":
         otp = request.POST.get("otp")
         user_obj = User.objects.get(id=user_id)
+        if cache.get(user_obj.phone_number):
+            data = cache.get(user_obj.phone_number)
+            if data['count'] >= 3:
+                messages.error(request, "You can request OTP after 5 mins.")
+                return redirect('/') 
         if user_obj.otp == int(otp):
             messages.success(request, "OTP verified successfully")
             login(request, user_obj)
