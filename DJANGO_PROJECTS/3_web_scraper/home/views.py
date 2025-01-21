c6d5a09a87e7c591c5d32da591d2f4be254d9f89
Ayush29Ayush <ayushsenapati123@gmail.com>
2025-01-21 13:16:44 +0530

    feat: Django Views added

--- DJANGO_PROJECTS/3_web_scraper/home/views.py
+++ DJANGO_PROJECTS/3_web_scraper/home/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from home.script import scrape_imdb_news
+from home.models import News
+from django.http import JsonResponse
+
 
 # Create your views here.
+def run_scraper(request):
+    scrape_imdb_news()
+    return JsonResponse({"status": True, "message": "Scraper executed successfully."})
+
+def index(request):
+    context = {
+        "news_data": News.objects.all()
+    }
+    return render(request, "index.html", context=context)
