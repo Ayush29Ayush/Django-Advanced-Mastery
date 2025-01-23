ea35e2cc4a694709ac24e5e1a064c891757fe0fd
Ayush29Ayush <ayushsenapati123@gmail.com>
2025-01-24 01:11:59 +0530

    feat: Celery setup completed

--- DJANGO_PROJECTS/3_web_scraper/home/urls.py
+++ DJANGO_PROJECTS/3_web_scraper/home/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from home.views import index, run_scraper
+from home.views import index, run_scraper, celery_task_testing
 
 urlpatterns = [
     path('', index, name="index"),
-    path('run-scraper/', run_scraper, name="run_scraper")
+    path('run-scraper/', run_scraper, name="run_scraper"),
+    path('celery-task-testing/', celery_task_testing, name="celery_task_testing")
 ]
--- DJANGO_PROJECTS/3_web_scraper/home/views.py
+++ DJANGO_PROJECTS/3_web_scraper/home/views.py
@@ -1,8 +1,10 @@
+from unittest import result
 from django.shortcuts import render
-from home.script import scrape_imdb_news
+# from home.script import scrape_imdb_news
+from home.script_for_celery import scrape_imdb_news
 from home.models import News
 from django.http import JsonResponse
-
+from home.tasks import add
 
 # Create your views here.
 def run_scraper(request):
@@ -14,3 +16,20 @@ def index(request):
         "news_data": News.objects.all()
     }
     return render(request, "index.html", context=context)
+
+def celery_task_testing(request):
+    # result = add(2, 3) #! This will run the task synchronously
+    # Call the Celery task asynchronously
+    result = add.delay(2, 3)  #! This will run the task asynchronously using Celery
+
+    # Return task details (task ID, status, and result once completed)
+    response_data = {
+        "task_id": result.id,
+        "status": result.status,  # This shows the status of the task (e.g., 'PENDING', 'SUCCESS')
+    }
+
+    # If the task is done, add the result to the response
+    if result.ready():  # Check if the task is finished
+        response_data["result"] = result.result  # Add the task result
+
+    return JsonResponse(response_data)
