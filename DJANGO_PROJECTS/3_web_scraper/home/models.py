31abc738ae9d58d38b09e41fe519d7a6bf7190e7
Ayush29Ayush <ayushsenapati123@gmail.com>
2025-01-24 14:50:57 +0530

    feat: New models added

--- DJANGO_PROJECTS/3_web_scraper/home/models.py
+++ DJANGO_PROJECTS/3_web_scraper/home/models.py
@@ -10,3 +10,28 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+    
+class UserInfo(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    age = models.IntegerField()
+    
+    # Gender choices
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+        ('P', 'Prefer not to say'),
+    ]
+    
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    is_adult = models.BooleanField(default=False)  
+
+    def save(self, *args, **kwargs):
+        # Automatically set is_adult based on age
+        self.is_adult = self.age >= 18
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
